a351021d6643fb7034199d2ea13ac48c05e2bcf1
adham2023 <adham.muhammadjonov1923@gmail.com>
2021-12-29 07:18:26 +0500

    asd

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Adham2023/socket-board.git main:gh-pages
+git push -f git@github.com:Adham2023/socket-board.git master:gh-pages
 cd -
